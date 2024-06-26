9f28eb5f8d6d831391884e06d9565bae67783f4d
redpandabun <144318643+redpandabun@users.noreply.github.com>
2024-06-26 18:15:06 +0900

    feat: add Sanitize toString utility class

---
+++ src/main/kotlin/com/mysolution/common/secret/SecretString.kt
@@ -0,0 +1,16 @@
+package com.mysolution.common.secret
+
+/**
+ * 외부로 노출되면 안되는 문자열 클래스
+ *
+ * 내부적으로 [toString]을 오버라이드하여 노출을 최소화한다.
+ *
+ * @author RedPandaBun
+ * @since 0.1.0
+ */
+@JvmInline
+value class SecretString(val value: String) {
+  constructor(value: CharSequence) : this(value.toString())
+
+  override fun toString(): String = "******"
+}
